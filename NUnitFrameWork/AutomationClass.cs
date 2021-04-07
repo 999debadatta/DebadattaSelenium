1a1ee4779ade54db0ec9bd2af04ebdaaa7ec36fc
999debadatta <222105debadatta@gmail.com>
2021-04-08 01:35:52 +0530

    Selenium Changes

---
+++ NUnitFrameWork/AutomationClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitFrameWork
+{
+    class AutomationClass
+    {
+        [SetUp]
+        public void ProgrameSetUp()
+        {
+            Console.Out.WriteLine("SetUP Function");
+
+        }
+        [Test]
+        public void ProgrameTest()
+        {
+
+            Console.WriteLine("Test Function");
+            
+
+        }
+        [TearDown]
+        public void ProgrameTeardown()
+        {
+            Console.Out.WriteLine("Teardown Function");
+
+        }
+
+
+    }
+}
---
+++ Newpro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newpro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("JAI Jagannath");
+                        int a = 10;
+            while (a < 20)
+            {
+                Console.WriteLine(a);
+                a += 1;                
+            }
+
+            for (int b = 1; b < 20; b++)
+            {
+                Console.WriteLine(b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
